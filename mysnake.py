01b4693063925156cd0b6b26cc2b66fc6b074509
dizazta <khalilzaman@hotmail.com>
2018-02-16 17:19:28 +0000

    Initial Commit

---
+++ mysnake.py
@@ -0,0 +1,171 @@
+import sys, pygame
+import numpy as np
+from random import randint
+
+
+pygame.init()
+grid = [10, 10]
+size = width, height = 300, 300
+speed = [2, 2]
+red = 255, 0, 0
+green = 132, 255, 132
+blue = 0, 0, 255
+darkBlue = 0, 0, 128
+white = 255, 255, 255
+black = 0, 0, 0
+pink = 255, 200, 200
+grey = 204, 204, 204
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+data = [0]*100
+
+foodpos = randint(0, 99)
+headpos = 0
+
+clear = 0
+head = 1
+bod = 2
+food = 3
+
+score = 1
+
+data[headpos] = head
+data[foodpos] = food
+# Head is 1
+# Body is 2
+# Food is 3
+
+facing = 'd'
+add = 0
+body = []
+
+def background():
+    global screen, grey, width, height
+
+    # Vertical lines
+    for xp in range(0, width, int(width/grid[0])):
+        pygame.draw.line(screen, grey, (xp, 0), (xp, height))
+
+    # Horizontal lines
+    for yp in range(0, height, int(height/grid[1])):
+        pygame.draw.line(screen, grey, (0, yp), (width, yp))
+
+
+def draw_grid():
+    global screen, black, width, height
+    xpos = 0
+    ypos = 0
+    xc = 0
+    yc = 0
+    w = width/grid[0]
+    h = height/grid[1]
+    for i in data:
+        if i == 1 or i == 2:
+            pygame.draw.rect(screen, black, (xpos, ypos, w, h))
+        elif i == 3:
+            pygame.draw.rect(screen, green, (xpos, ypos, w, h))
+
+        xc += 1
+        if xc % grid[0] == 0:
+            xc = 0
+            yc += 1
+        xpos = (xc % grid[0]) * w
+        ypos = (yc % grid[1]) * h
+
+
+def update_body():
+    global facing, headpos, data, head, body, add
+    if len(body) != 0:
+        if add == 0:
+            data[body[len(body) - 1]] = clear
+        add = 0
+        body = [headpos] + body[:-1]
+        data[headpos] = bod
+    else:
+        data[headpos] = 0
+
+
+def update_position():
+    global facing, headpos, data, head
+    if facing == 'd':
+        if headpos >= ((grid[0] * grid[1]) - grid[0]):
+            sys.exit()
+        else:
+            headpos += grid[1]
+            # Check for self collision
+            if data[headpos] == bod:
+                sys.exit()
+            data[headpos] = head
+    elif facing == 'l':
+        if headpos % grid[0] == 0:
+            sys.exit()
+        else:
+            headpos -= 1
+            # Check for self collision
+            if data[headpos] == bod:
+                sys.exit()
+            data[headpos] = head
+    elif facing == 'r':
+        if (headpos+1) % grid[0] == 0:
+            sys.exit()
+        else:
+            headpos += 1
+            # Check for self collision
+            if data[headpos] == bod:
+                sys.exit()
+            data[headpos] = 1
+    elif facing == 'u':
+        if headpos < grid[1]:
+            sys.exit()
+        else:
+            headpos -= grid[1]
+            # Check for self collision
+            if data[headpos] == bod:
+                sys.exit()
+            data[headpos] = 1
+
+
+def increase_body():
+    global body, headpos, foodpos, data, food, add
+    if len(body) == 0:
+        body.append(headpos)
+    else:
+        body.append(body[0])
+    foodpos = randint(0, 99)
+    while data[foodpos] != 0:
+        foodpos = randint(0, 99)
+    data[foodpos] = food
+    add = 1
+
+
+def update_score():
+    global score
+    score += 1
+    print("Score is:", score)
+
+
+while 1:
+    clock.tick(3)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                facing = 'd'
+            elif event.key == pygame.K_LEFT:
+                facing = 'l'
+            elif event.key == pygame.K_RIGHT:
+                facing = 'r'
+            elif event.key == pygame.K_UP:
+                facing = 'u'
+    update_body()
+    update_position()
+    if headpos == foodpos:
+        update_score()
+        increase_body()
+    screen.fill(white)
+    background()
+    draw_grid()
+    print(data)
+    pygame.display.update()
---
+++ mysnakeAI.py
@@ -0,0 +1,287 @@
+import numpy as np
+from random import randint, uniform as randfloat
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+np.set_printoptions(threshold=np.nan)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+class SnakeGame:
+
+    def __init__(self):
+
+        self.clear = 0
+        self.head = 1
+        self.bod = 2
+        self.food = 3
+
+        self.data = [self.clear] * 100
+        self.foodpos = randint(0, 99)
+        self.headpos = 0
+
+        self.data[self.headpos] = self.head
+        self.data[self.foodpos] = self.food
+
+        self.score = 1
+        self.increase_tail = 0 # For when something was just eaten
+        self.body = []
+
+        # 1 - Down, 2 - Left, 3 - Right, 4 - Up
+        self.facing = 1
+        self.ended = False
+
+    def update_body(self):
+        if len(self.body) != 0:
+            if self.increase_tail == 0:
+                self.data[self.body[len(self.body) - 1]] = self.clear
+            self.increase_tail = 0
+            self.body = [self.headpos] + self.body[:-1]
+            self.data[self.headpos] = self.bod
+        else:
+            self.data[self.headpos] = 0
+
+    def check_for_collision(self):
+        if self.data[self.headpos] == self.bod:
+            self.ended = True
+
+    def update_position(self):
+        if self.facing == 1: # Down
+            if self.headpos >= 90:
+                self.ended = True
+            else:
+                self.headpos += 10
+        elif self.facing == 2: # Left
+            if self.headpos % 10 == 0:
+                self.ended = True
+            else:
+                self.headpos -= 1
+        elif self.facing == 3: # Right
+            if (self.headpos + 1) % 10 == 0:
+                self.ended = True
+            else:
+                self.headpos += 1
+        elif self.facing == 4: # Up
+            if self.headpos < 10:
+                self.ended = True
+            else:
+                self.headpos -= 10
+
+        self.check_for_collision()
+        self.data[self.headpos] = self.head
+
+    def increase_body(self):
+
+        if len(self.body) == 0:
+            self.body.append(self.headpos)
+        else:
+            self.body.append(self.body[0])
+
+        self.foodpos = randint(0, 99)
+        while self.data[self.foodpos] != 0:
+            self.foodpos = randint(0, 99)
+
+        self.data[self.foodpos] = self.food
+        self.add = 1
+
+    def update_score(self):
+        self.score += 1
+
+    def play(self, face=0):
+        x = 0
+        y = 0
+        if face == 0:
+            face = self.facing
+        self.facing = face
+        self.update_body()
+        self.update_position()
+        if self.headpos == self.foodpos:
+            self.update_score()
+            self.increase_body()
+
+    def output(self):
+        if self.ended is False:
+            return np.asarray(self.data + [self.facing, self.increase_tail])
+        return False
+
+
+class SnakeNeuralNetwork:
+
+    def __init__(self, w1=None, w2=None, w3=None, w4=None):
+        self.game = SnakeGame()
+        self.INPUT = self.game.output()
+        self.bias_term = 1
+        self.INPUT = np.append(self.INPUT, self.bias_term)
+        self.played = []
+
+        if w1 is None:
+            self.weights1 = np.random.rand(102, 103)
+            self.weights2 = np.random.rand(102, 103)
+            self.weights3 = np.random.rand(102, 103)
+            self.weights4 = np.random.rand(4, 103)
+        else:
+            self.weights1 = w1
+            self.weights2 = w2
+            self.weights3 = w3
+            self.weights4 = w4
+
+        self.H = np.random.rand(102, 1)
+        self.facing = 1
+        self.output = 0
+        self.ended = self.game.ended
+        self.alive = 0
+        self.fitness = 0
+
+    def feed_forward(self):
+        h = sigmoid(np.dot(self.weights1, self.INPUT.T))
+        self.H = np.append(h, self.bias_term)
+        h = sigmoid(np.dot(self.weights2, self.H.T))
+        self.H = np.append(h, self.bias_term)
+        h = sigmoid(np.dot(self.weights3, self.H.T))
+        self.H = np.append(h, self.bias_term)
+        self.output = sigmoid(np.dot(self.weights4, self.H.T))
+
+    def face(self):
+        facing = 0
+        if self.output[0] > 0.5 and facing == 0:
+            facing = 1
+        if self.output[1] > 0.5 and facing == 0:
+            facing = 2
+        if self.output[2] > 0.5 and facing == 0:
+            facing = 3
+        if self.output[3] > 0.5 and facing == 0:
+            facing = 4
+        self.facing = facing
+        self.played.append(facing)
+
+    def get_fitness(self):
+        self.fitness = self.game.score * (self.alive)
+        self.fitness += 10*(max(self.output) - min (self.output))
+
+
+    def play(self):
+        while self.game.ended is False:
+            self.feed_forward()
+            self.face()
+            self.game.play(self.facing)
+            self.ended = self.game.ended
+            self.alive += 1
+        self.get_fitness()
+        return self.fitness
+
+    def get_weights(self):
+        return self.weights1.flatten(), self.weights2.flatten(), self.weights3.flatten(), self.weights4.flatten()
+
+
+mutation_rate = 0.5
+def breed(S1, S2):
+    w11, w21, w31, w41 = S1.get_weights()
+    w12, w22, w32, w42 = S2.get_weights()
+    w1 = generate_weights(w11, w12, (102, 103))
+    w2 = generate_weights(w21, w22, (102, 103))
+    w3 = generate_weights(w31, w32, (102, 103))
+    w4 = generate_weights(w41, w42, (4, 103))
+    return w1, w2, w3, w4
+
+def generate_weights(mother_w, father_w, shape):
+    child_w = []
+    for i in range(len(mother_w)):
+        mf = randint(0, 1)
+        mutation = randfloat(0, 1)
+
+        change = 0
+        if mutation_rate + mutation >= 1:
+            change = randfloat(-1, 1)
+
+        if mf == 0:
+            child_w.append(mother_w[i] + change)
+        else:
+            child_w.append(father_w[i] + change)
+    return np.asarray(child_w).reshape(shape)
+
+
+def generate_population():
+    global Snakes
+    for _ in range(25):
+        Snakes.append(SnakeNeuralNetwork())
+
+
+def survival_of_the_fittest():
+    global Snakes
+    breeding_group = [0] * 10
+    max_fitness = 0
+    best_snake = None
+    for i in range(len(Snakes)):
+        snake = Snakes[i]
+        snake.play()
+        if snake.fitness > min(breeding_group):
+            breeding_group[breeding_group.index(min(breeding_group))] = i
+            if snake.fitness > max_fitness:
+                max_fitness = snake.fitness
+                best_snake = snake
+    print(best_snake.played, best_snake.output, max_fitness)
+    return breeding_group
+
+
+
+def population_breeding():
+    global Snakes, selective_breeding
+    newSnakes = []
+    for _ in range(25):
+        m = randint(0, 9)
+        f = randint(0, 9)
+        while f == m:
+            f = randint(0, 9)
+        w1, w2, w3, w4 = breed(Snakes[selective_breeding[m]], Snakes[selective_breeding[f]])
+        child = SnakeNeuralNetwork(w1, w2, w3, w4)
+        newSnakes.append(child)
+    return newSnakes
+
+
+def save():
+    global Snakes
+    snakebrains = "snake1"
+    counter = 1
+    for snake in Snakes:
+
+        w1, w2, w3, w4 = snake.get_weights()
+        w1.dump(snakebrains + "w1" + ".dat")
+        w2.dump(snakebrains + "w2" + ".dat")
+        w3.dump(snakebrains + "w3" + ".dat")
+        w4.dump(snakebrains + "w4" + ".dat")
+
+        counter += 1
+        snakebrains = snakebrains[:5] + str(counter)
+
+
+def load():
+    global Snakes
+    snakebrains = "snake1"
+    counter = 1
+    for _ in range(25):
+
+        w1 = np.load(snakebrains + "w1" + ".dat")
+        w2 = np.load(snakebrains + "w2" + ".dat")
+        w3 = np.load(snakebrains + "w3" + ".dat")
+        w4 = np.load(snakebrains + "w4" + ".dat")
+
+        Snakes.append(SnakeNeuralNetwork(w1.reshape(102, 103), w2.reshape(102, 103), w3.reshape(102, 103), w4.reshape(4, 103)))
+        counter += 1
+        snakebrains = snakebrains[:5] + str(counter)
+
+
+Snakes = []
+#generate_population()
+load()
+
+counter = 0
+while True:
+    selective_breeding = survival_of_the_fittest()
+    Snakes = population_breeding()
+    counter += 1
+    if counter % 10 == 0:
+        print("Saving ...", end=" ")
+        save()
+        print("Saved")
+
